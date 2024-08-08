5f4904ad5bc6ed3ea8264e14a1aeccd2704c490e
Dokka Ilaev <ilaev.dokka@gmail.com>
2024-08-08 11:42:25 +0300

    initial

--- module_4_1.py
+++ module_4_1.py
@@ -1,10 +1,10 @@
-from fake_math4_1 import divide as f_div
-from true_math4_1 import divide as t_div
+from fake_math4_1 import divide as fake_math
+from true_math4_1 import divide as true_math
 
-result1 = f_div(69, 3)
-result2 = f_div(3, 0)
-result3 = t_div(49, 7)
-result4 = t_div(15, 0)
+result1 = fake_math(69, 3)
+result2 = fake_math(3, 0)
+result3 = true_math(49, 7)
+result4 = true_math(15, 0)
 
 print(result1)
 print(result2)
