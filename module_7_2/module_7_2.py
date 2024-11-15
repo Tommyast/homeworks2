8f22dbbdb86bffc966577cef157549089ed1998e
Dokka Ilaev <ilaev.dokka@gmail.com>
2024-11-15 13:32:32 +0300

    initial

---
+++ module_7_2/module_7_2.py
@@ -0,0 +1,25 @@
+def custom_write(file_name, strings):
+    file = open('module_7_2.txt', 'w', encoding='utf-8')
+
+    strings_position = {}
+
+    for index, string in enumerate(strings):
+        position = file.tell()
+
+        file.write(string + '\n')
+        strings_position[(index + 1, position)] = string
+
+    file.close()
+    return strings_position
+
+info = [
+    'Text for tell.',
+    'Используйте кодировку utf-8.',
+    'Because there are 2 languages!',
+    'Спасибо!'
+    ]
+
+result = custom_write('test.txt', info)
+for elem in result.items():
+  print(elem)
+
