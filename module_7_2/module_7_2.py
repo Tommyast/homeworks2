126160d1390447499304cfc413e3b0aba4579246
Dokka Ilaev <ilaev.dokka@gmail.com>
2024-11-15 13:32:48 +0300

    initial

--- module_7_2/module_7_2.py
+++ module_7_2/module_7_2.py
@@ -12,14 +12,14 @@ def custom_write(file_name, strings):
     file.close()
     return strings_position
 
+
 info = [
     'Text for tell.',
     'Используйте кодировку utf-8.',
     'Because there are 2 languages!',
     'Спасибо!'
-    ]
+]
 
 result = custom_write('test.txt', info)
 for elem in result.items():
-  print(elem)
-
+    print(elem)
